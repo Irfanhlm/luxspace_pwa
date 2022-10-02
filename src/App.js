076b4e4ae7e22bc1434f6ac82cd376c049b19d65
Irfan Halim <irfanhalimhidayat@gmail.com>
2022-10-03 03:18:25 +0700

    add Modal component and function Modal

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import Arrived from "./components/Arrived";
 import Browse from "./components/Browse";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Clients from "./components/Clients";
+import AsideMenu from "./components/AsideMenu";
+import Footer from "./components/Footer";
+import Modal from "./components/Modal";
 
 function App() {
   return (
@@ -11,6 +15,10 @@ function App() {
       <Hero />
       <Browse />
       <Arrived />
+      <Clients />
+      <AsideMenu />
+      <Footer />
+      <Modal />
     </>
   );
 }
